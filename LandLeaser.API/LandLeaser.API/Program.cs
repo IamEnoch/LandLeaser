2d9fc679c01f3d344ab7b68e44d938f5026a3778
Tech Wiz <enoch.mwangi@outlook.com>
2022-09-02 20:25:36 +0300

    Connected database to the application

--- LandLeaser.API/LandLeaser.API/Program.cs
+++ LandLeaser.API/LandLeaser.API/Program.cs
@@ -1,7 +1,14 @@
+using LandLeaser.API.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    //Configure DbContext with sql
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
