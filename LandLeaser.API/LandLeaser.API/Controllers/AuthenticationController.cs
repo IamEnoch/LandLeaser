452cd490a50289034b0492df4911d975c976d5e9
Tech Wiz <enoch.mwangi@outlook.com>
2022-09-03 11:30:12 +0300

    Setup the authentication controller

---
+++ LandLeaser.API/LandLeaser.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,26 @@
+﻿using LandLeaser.API.Data;
+using LandLeaser.API.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LandLeaser.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly AppDbContext _context;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager, AppDbContext context, RoleManager<ApplicationRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _context = context;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+    }
+}
