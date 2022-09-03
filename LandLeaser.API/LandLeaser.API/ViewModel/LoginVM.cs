29a0d66d160b7f0bb38263ae3ccc8987b373d4e2
Tech Wiz <enoch.mwangi@outlook.com>
2022-09-03 12:34:50 +0300

    Created the login view model view model

---
+++ LandLeaser.API/LandLeaser.API/ViewModel/LoginVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LandLeaser.API.ViewModel
+{
+    public class LoginVM
+    {
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
