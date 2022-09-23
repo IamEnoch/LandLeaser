62f037da87b2ab6a905905488f06c82fe5e04135
Tech Wiz <enoch.mwangi@outlook.com>
2022-09-23 11:18:37 +0300

    Created loading page view model

---
+++ LandLeaser.APP/ViewModels/LoadingPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LandLeaser.APP.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandLeaserApp.ViewModels
+{
+    public partial class LoadingPageViewModel : BaseViewModel
+    {
+    }
+}
