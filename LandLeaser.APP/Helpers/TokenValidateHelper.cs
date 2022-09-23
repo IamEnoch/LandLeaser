b9b0e8138915c5eee5abbeaf353f69fb69fd7199
Tech Wiz <enoch.mwangi@outlook.com>
2022-09-23 16:35:03 +0300

    Created token validate helper class

---
+++ LandLeaser.APP/Helpers/TokenValidateHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandLeaserApp.Helpers
+{
+    public class TokenValidateHelper
+    {
+        public bool ValidateToken(string accessToken)
+        {
+            //Create JwtSecurityTokenHandler
+            var handler = new JwtSecurityTokenHandler();
+
+            var token = handler.ReadJwtToken(accessToken);
+
+            if(token.ValidTo < DateTime.UtcNow)
+            {
+                return false; 
+            }
+            return true;
+        }
+    }
+}
