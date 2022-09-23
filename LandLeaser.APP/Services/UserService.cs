6b1206d4a297f11135edcc60d8916e0b1c90c41f
Tech Wiz <enoch.mwangi@outlook.com>
2022-09-23 16:36:09 +0300

    Updated get user method to include access token validity check before making api call

--- LandLeaser.APP/Services/UserService.cs
+++ LandLeaser.APP/Services/UserService.cs
@@ -7,18 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http.Headers;
+using LandLeaserApp.Helpers;
+using LandLeaser.APP;
 
 namespace LandLeaserApp.Services
 {
     public class UserService : IUserService
     {
-        public HttpClient Client = new HttpClient();   
+        private readonly ILoginService _loginService;
+        public HttpClient Client = new HttpClient(); 
+        public TokenValidateHelper TokenValidater = new TokenValidateHelper();
+
+        public UserService(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        /// <summary>
+        /// Get user method
+        /// </summary>
+        /// <param name="email">Email of the particular user</param>
+        /// <param name="accessToken">Access token belonging to the user</param>
+        /// <returns>User basic information(Full name, email and phone number)</returns>
         public async Task<UserBasicInfo> GetUser(string email, string accessToken)
         {
             //Url for the desired endpoint
             var url = $"https://landleaserapi.azurewebsites.net/api/Users/{email}";
 
+            //Validate the token
+            var validToken = await TokenValidater.ValidateToken(accessToken);
 
+            //Refresh token
+            if(validToken != true)
+            {
+                var tokenRequest = new TokenRequest()
+                {
+                    Token = accessToken,
+                    RefreshToken = Preferences.Get(nameof(App.RefreshToken), "")
+                };
+
+                var tokenRefreshResult = await _loginService.RefreshToken(tokenRequest);
+                accessToken = tokenRefreshResult.Token;
+
+                //Set the new preferences
+                Preferences.Set(nameof(App.Token), tokenRefreshResult.Token);
+                Preferences.Set(nameof(App.RefreshToken), tokenRefreshResult.RefreshToken);
+
+            }
 
             Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
