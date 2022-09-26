149c2d4858eecc3f0f6ffc5b6092acad825969e7
Tech Wiz <enoch.mwangi@outlook.com>
2022-09-26 22:30:06 +0300

    Updated sign up page UI

--- LandLeaser.APP/MauiProgram.cs
+++ LandLeaser.APP/MauiProgram.cs
@@ -23,6 +23,7 @@ namespace LandLeaser.APP
             //Services
             builder.Services.AddSingleton<ILoginService, LoginService>();
             builder.Services.AddSingleton<IUserService, UserService>();
+            builder.Services.AddSingleton<IRegisterService, RegisterService>();
 
             //Views
             builder.Services.AddSingleton<LoadingPage>();
