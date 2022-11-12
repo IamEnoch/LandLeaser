5a303e2f9d4da90ea63234f5e7f9eb27bad0cb49
Tech Wiz <enoch.mwangi@outlook.com>
2022-11-12 12:51:49 +0300

    Added migrations and database update

---
+++ LandLeaser.API/LandLeaser.API/Migrations/20221112090555_updatedListingTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LandLeaser.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedListingTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Listings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageId",
+                table: "Listings",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
