359edcfe4ac0fa73a5a00afdf61a90492d03ecfe
Tech Wiz <enoch.mwangi@outlook.com>
2022-11-13 16:47:34 +0300

    Refactoring

--- LandLeaser.APP/ViewModels/LoadingPageViewModel.cs
+++ LandLeaser.APP/ViewModels/LoadingPageViewModel.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LandLeaserApp.Helpers;
+using LandLeaserApp.Views;
 
 namespace LandLeaserApp.ViewModels
 {
@@ -19,76 +20,102 @@ namespace LandLeaserApp.ViewModels
     {
         private readonly ILoginService _loginService;
         private readonly IUserService _userService;
+        public string UserDetailsStr { get; set; }
+
         public TokenValidateHelper TokenValidater = new TokenValidateHelper();
         public LoadingPageViewModel(ILoginService loginService, IUserService userService)
         {
             _loginService = loginService;
             _userService = userService;
-            CheckUserLoginDetails();
-        }
-        async Task CheckUserLoginDetails()
-        {
-            string userDetailsStr = Preferences.Get(nameof(App.UserInfo), "");
-            string accessToken = await SecureStorage.GetAsync(nameof(App.Token));
-            string refreshToken = await SecureStorage.GetAsync(nameof(App.RefreshToken));
+            Check();
 
-            if (string.IsNullOrWhiteSpace(userDetailsStr))
+        }
+
+        /// <summary>
+        /// Method to check and validate stored credentials. Called in the Loading page View model
+        /// </summary>
+        /// <returns></returns>
+        public async Task Check()
+        {
+            IsBusy = true;
+
+            var result = await CheckUserLoginDetails();
+
+            if (result == true)
             {
-                // navigate to Login Page
-                await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");                
+                string accessToken = await SecureStorage.GetAsync(nameof(App.Token));
+                string refreshToken = await SecureStorage.GetAsync(nameof(App.RefreshToken));
+                var response = await ValidateAndRefresh(accessToken, refreshToken, UserDetailsStr);
+
+                if (response == true)
+                    // navigate to home Page
+                    await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+                else
+                    // navigate to Login Page
+                    await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+
+
             }
             else
+                // navigate to Login Page
+                await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+
+            IsBusy = false;
+        }
+        public async Task<bool> CheckUserLoginDetails()
+        {
+            UserDetailsStr = Preferences.Get(nameof(App.UserInfo), "");
+            
+
+            if (string.IsNullOrWhiteSpace(UserDetailsStr))
+                return false;
+            
+            return true;
+        }
+
+        public async Task<bool> ValidateAndRefresh(string accessToken, string refreshToken, string userDetailsStr)
+        {
+            //Validate the token
+            var validToken = await TokenValidater.ValidateToken(accessToken);
+            if (validToken != true)
             {
-                IsBusy = true;
-
-                //Validate the token
-                var validToken = await TokenValidater.ValidateToken(accessToken);
-                if(validToken != true)
+                var tokenRequest = new TokenRequest
                 {
-                    var tokenRequest = new TokenRequest
-                    {
-                        Token = accessToken,
-                        RefreshToken = refreshToken
-                    };
+                    Token = accessToken,
+                    RefreshToken = refreshToken
+                };
 
-                    
-                    //call refresh token method
-                    var response = await _loginService.RefreshToken(tokenRequest);
-                    if(response == null)
-                    {
-                        IsBusy = false;
-                        await Shell.Current.DisplayAlert("Fail", "Refreshing token unsuccessful", "ok");
-                        await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
-                    }
-                    else
-                    {
-                        //Update user preferences and securely store token
-                        await SecureStorage.SetAsync(nameof(App.Token), response.Token);
-                        await SecureStorage.SetAsync(nameof(App.RefreshToken), response.RefreshToken);
 
-                        App.Token = response.Token;
-                        App.RefreshToken = response.RefreshToken;
-
-                        IsBusy = false;
-
-                        await Shell.Current.DisplayAlert("Success", "No login required", "ok");
-                        await Shell.Current.GoToAsync($"{nameof(PushPage)}");
-                    }                    
+                //call refresh token method
+                var response = await _loginService.RefreshToken(tokenRequest);
+                if (response == null)
+                {
+                    await Shell.Current.DisplayAlert("Fail", "Refreshing token unsuccessful", "ok");
+                    return false;
                 }
                 else
                 {
-                    var userInfo = JsonConvert.DeserializeObject<UserBasicInfo>(userDetailsStr);
+                    //Update user preferences and securely store token
+                    await SecureStorage.SetAsync(nameof(App.Token), response.Token);
+                    await SecureStorage.SetAsync(nameof(App.RefreshToken), response.RefreshToken);
 
-                    App.UserInfo = userInfo;
-                    App.Token = accessToken;
-                    App.RefreshToken = refreshToken;
-
-                    IsBusy = false;
-
+                    App.Token = response.Token;
+                    App.RefreshToken = response.RefreshToken;
+                    
                     await Shell.Current.DisplayAlert("Success", "No login required", "ok");
-                    await Shell.Current.GoToAsync($"{nameof(PushPage)}");
+                    return true;
                 }
+            }
+            else
+            {
+                var userInfo = JsonConvert.DeserializeObject<UserBasicInfo>(userDetailsStr);
 
+                App.UserInfo = userInfo;
+                App.Token = accessToken;
+                App.RefreshToken = refreshToken;
+                
+                await Shell.Current.DisplayAlert("Success", "No login required", "ok");
+                return true;
             }
         }
     }
