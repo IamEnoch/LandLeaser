5e2d0106bcac1529f8e6891c9bbe96c6cc5c3cc5
Tech Wiz <enoch.mwangi@outlook.com>
2022-11-13 16:46:35 +0300

    Created response message class to store details after making api calls

---
+++ LandLeaser.Shared/Models/ResponseMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace LandLeaser.Shared.Models
+{
+    public class ResponseMessage
+    {
+        public Object ResponseItem { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccessful { get; set; }
+
+        public ResponseMessage(object responseItem, HttpStatusCode statusCode, bool isSuccessful)
+        {
+            ResponseItem = responseItem;
+            StatusCode = statusCode;    
+            IsSuccessful = isSuccessful;
+        }
+
+    }
+}
