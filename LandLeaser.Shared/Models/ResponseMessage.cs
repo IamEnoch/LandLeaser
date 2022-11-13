a59071ec1566d806475511cc69b744bcc918260d
Tech Wiz <enoch.mwangi@outlook.com>
2022-11-13 23:30:39 +0300

    Created response message for http call result

--- LandLeaser.Shared/Models/ResponseMessage.cs
+++ LandLeaser.Shared/Models/ResponseMessage.cs
@@ -7,13 +7,11 @@ namespace LandLeaser.Shared.Models
 {
     public class ResponseMessage
     {
-        public Object ResponseItem { get; set; }
         public HttpStatusCode StatusCode { get; set; }
         public bool IsSuccessful { get; set; }
 
-        public ResponseMessage(object responseItem, HttpStatusCode statusCode, bool isSuccessful)
+        public ResponseMessage(HttpStatusCode statusCode, bool isSuccessful)
         {
-            ResponseItem = responseItem;
             StatusCode = statusCode;    
             IsSuccessful = isSuccessful;
         }
