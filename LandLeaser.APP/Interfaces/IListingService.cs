7740110efe9187b25e165320d257db29e0ad7df2
Tech Wiz <enoch.mwangi@outlook.com>
2022-11-13 13:34:36 +0300

    Created and implemeted IListing service

---
+++ LandLeaser.APP/Interfaces/IListingService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LandLeaser.Shared.DTOs;
+
+namespace LandLeaserApp.Interfaces
+{
+    interface IListingService
+    {
+        /// <summary>
+        /// Method that gets all listings
+        /// </summary>
+        /// <param name="authToken">Bearer authentication token</param>
+        /// <returns></returns>
+        public Task <List<GetListingDto>> GetListingsAsync (string authToken);
+
+        /// <summary>
+        /// Gets a specific listing
+        /// </summary>
+        /// <param name="authToken">Bearer authentication token</param>
+        /// <param name="id">Listing id</param>
+        /// <returns></returns>
+        public Task<GetListingDto> GetListingAsync(string authToken, string id);
+    }
+}
---
+++ LandLeaser.APP/Services/ListingService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LandLeaser.Shared.DTOs;
+using LandLeaserApp.Helpers;
+using LandLeaserApp.Interfaces;
+
+namespace LandLeaserApp.Services
+{
+    public class ListingService : IListingService
+    {
+        public HttpDataHelper Client { get; set; }
+
+        public String BaseAddress = "https://landleaserapi.azurewebsites.net/";
+
+        public async Task<List<GetListingDto>> GetListingsAsync(string authToken)
+        {
+            string endpoint = "api/listings";
+            Client = new HttpDataHelper(BaseAddress, authToken);
+
+            var response = await Client.GetAsync<List<GetListingDto>>(endpoint);
+
+            return (List<GetListingDto>)response.ResponseItem;
+
+        }
+
+        public async Task<GetListingDto> GetListingAsync(string authToken, string id)
+        {
+            string endpoint = $"api/listing/{id}";
+            Client = new HttpDataHelper(BaseAddress, authToken);
+
+            var response = await Client.GetAsync<GetListingDto>(endpoint);
+
+            return (GetListingDto)response.ResponseItem;
+        }
+    }
+}
