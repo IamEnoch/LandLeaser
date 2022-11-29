db9b5e565c982cef59df022d8c329680ecacae32
Tech Wiz <enoch.mwangi@outlook.com>
2022-11-29 23:52:05 +0300

    Created logout page and view model

---
+++ LandLeaser.APP/ViewModels/LogoutViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandLeaser.APP.ViewModels
+{
+    public partial class LogoutViewModel : BaseViewModel
+    {
+        private AppShell _appShell;
+        public LogoutViewModel(AppShell appShell)
+        {
+            _appShell = appShell;
+        }
+
+        [RelayCommand]
+        public void Logout()
+        {
+            Preferences.Remove(nameof(App.UserInfo));
+            SecureStorage.RemoveAll();
+
+            App.Current.MainPage = _appShell;
+
+            //StateChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ LandLeaser.APP/Views/LogoutPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LandLeaser.APP.ViewModels;
+
+namespace LandLeaser.APP.Views;
+
+public partial class LogoutPage : ContentPage
+{
+    public LogoutPage(LogoutViewModel logoutViewModel)
+    {
+        InitializeComponent();
+        BindingContext = logoutViewModel;
+    }
+}
