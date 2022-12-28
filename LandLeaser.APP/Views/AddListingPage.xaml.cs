7a1223586959425a410ba891af81e2b2f71268df
Enoch Mwangi <enoch.mwangi@outlook.com>
2022-12-28 23:28:53 +0300

    Created add listing page

---
+++ LandLeaser.APP/Views/AddListingPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LandLeaser.APP.ViewModels;
+
+namespace LandLeaser.APP.Views;
+
+public partial class AddListingPage : ContentPage
+{
+    public AddListingPage(AddListingViewModel addListingViewModel)
+    {
+        BindingContext = addListingViewModel;
+        InitializeComponent();
+    }
+
+    protected override bool OnBackButtonPressed()
+    {
+        Shell.Current.GoToAsync("..");
+        return base.OnBackButtonPressed();
+    }
+}
