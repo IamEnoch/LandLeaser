f655ef8509834f5f9ca477473eda71d8af8f9a06
Enoch Mwangi <enoch.mwangi@outlook.com>
2022-12-28 23:28:18 +0300

    Created add listing view model

---
+++ LandLeaser.APP/ViewModels/AddListingViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace LandLeaser.APP.ViewModels
+{
+    public partial class AddListingViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        private string _landLocation;
+
+        [ObservableProperty]
+        private string _landSize;
+
+        [ObservableProperty]
+        private string _sizeMetric;
+
+        [ObservableProperty]
+        private string _landCost;
+
+        [ObservableProperty]
+        private string _currency;
+
+        [ObservableProperty]
+        private string _leaseDuration;
+
+        [ObservableProperty]
+        private string _durationMetric;
+
+        [ObservableProperty]
+        private string _description;
+
+        [ObservableProperty]
+        private string _preferredCrop;
+
+        
+    }
+}
