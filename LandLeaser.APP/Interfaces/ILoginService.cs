45bf2b313d28ab3440ce1aa83328d704ff84b7c7
Enoch Mwangi <enoch.mwangi@outlook.com>
2022-12-30 23:48:51 +0300

    Implemented the use of dto

--- LandLeaser.APP/Interfaces/ILoginService.cs
+++ LandLeaser.APP/Interfaces/ILoginService.cs
@@ -1,10 +1,11 @@
-﻿using LandLeaser.Shared.Models;
+﻿using LandLeaser.Shared.DTOs;
+using LandLeaser.Shared.Models;
 
 namespace LandLeaser.APP.Interfaces
 {
     public interface ILoginService
     {
-        public Task<LoginResult> Authenticate(LoginRequest loginRequest, string authToken = null);
+        public Task<LoginResultDto> Authenticate(LoginRequest loginRequest, string authToken = null);
         public Task<LoginResult> RefreshToken(TokenRequest tokenRequest, string authToken = null);
     }
 }
