cc2b2fed7329a00aeec116525f76705e18cdf4de
Enoch Mwangi <enoch.mwangi@outlook.com>
2022-12-30 23:49:17 +0300

    Code refactoring

---
+++ LandLeaser.Shared/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LandLeaser.Shared.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string? Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
