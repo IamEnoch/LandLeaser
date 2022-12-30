2b596e0fbb87fa9340adbc6765948e468d1ed552
Enoch Mwangi <enoch.mwangi@outlook.com>
2022-12-30 23:47:05 +0300

    ListingImageProfile to be used by auto mappper

---
+++ LandLeaser.API/Mapping/ListingImageProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LandLeaser.API.Models;
+using LandLeaser.Shared.DTOs;
+
+namespace LandLeaser.API.Mapping
+{
+    public class ListingImageProfile : Profile
+    {
+        public ListingImageProfile()
+        {
+            CreateMap<ListingImage, ListingImageDto>();
+            CreateMap<ListingImageDto, ListingImage>();
+        }
+    }
+}
